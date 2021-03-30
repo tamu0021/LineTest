fe8a2e7803e8ff04fe8cad0406b9116bcbf17ecb
Tamu0021 <czr29479@gmail.com>
2021-03-30 22:07:22 +0900

    Add Forecast Class

--- LineTest.py
+++ LineTest.py
@@ -6,6 +6,33 @@ Created on Wed Mar  3 21:47:20 2021
 """
 
 import requests
+from bs4 import BeautifulSoup
+import urllib.request
+
+class Forecast:
+    FORECAST_URL = "https://tenki.jp/forecast/6/30/6200/27100/"
+    response = urllib.request.urlopen(FORECAST_URL)
+    soup = BeautifulSoup(response, 'html.parser')
+    # 天気
+    weather = soup.find_all("p", class_="weather_telop")
+    # 最高気温
+    high_temp = soup.find_all("dd", class_="high-temp temp")
+    # 最低気温
+    low_temp = soup.find_all("dd", class_="low-temp temp")
+    # 降水確率
+    rainy_percent = soup.select("tr.rain-probability td")
+    # 日にち
+    date = soup.find_all("h3", class_="left-style")
+    def today_weather(self):
+        today_weather = self.date[0].getText() + "\n\n" + self.weather[0].getText()
+        today_high_temp = "\n最高" + self.high_temp[0].getText()
+        today_low_temp = "\n最低" + self.low_temp[0].getText()
+        today_rain_00_06 = "\n\n降水確率\n00-06時" + self.rainy_percent[0].getText()
+        today_rain_06_12 = "\n06-12時" + self.rainy_percent[1].getText()
+        today_rain_12_18 = "\n12-18時" + self.rainy_percent[2].getText()
+        today_rain_18_24 = "\n18-24時" + self.rainy_percent[3].getText()
+        
+        return (today_weather + today_high_temp + today_low_temp + today_rain_00_06 + today_rain_06_12 + today_rain_12_18 + today_rain_18_24)
 
 class LINENotifyBot:
     API_URL = 'https://notify-api.line.me/api/notify'
@@ -32,11 +59,14 @@ class LINENotifyBot:
             )
         
 
+forecast = Forecast()
+print(forecast.today_weather)
+
 bot = LINENotifyBot(access_token='Write your token.')
 
 bot.send(
     message='Write Your Message',
-    image='Test.png',  # png or jpg
+    image='Test.jpg',  # png or jpg
     sticker_package_id=1,
     sticker_id=13,
     )
